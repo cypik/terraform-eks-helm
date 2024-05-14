cc79c57834a4ab5ea8e72167b9ae8b448d00c79e
Sohan Yadav <sohanyadavdevops@gmail.com>
2024-05-14 19:16:43 +0530

    adding splunk module

---
+++ example/splunk/example.tf
@@ -0,0 +1,290 @@
+provider "aws" {
+  region = local.region
+}
+
+
+locals {
+  name           = "demo"
+  environment    = "testing"
+  region         = "us-west-2"
+  vpc_cidr_block = module.vpc.vpc_cidr_block
+
+}
+
+module "vpc" {
+  source      = "cypik/vpc/aws"
+  version     = "1.0.1"
+  name        = "${local.name}-vpc"
+  environment = local.environment
+  cidr_block  = "10.10.0.0/16"
+}
+
+
+module "subnets" {
+  source              = "cypik/subnet/aws"
+  version             = "1.0.2"
+  name                = "${local.name}-subnet"
+  environment         = local.environment
+  nat_gateway_enabled = true
+  single_nat_gateway  = true
+  availability_zones  = ["${local.region}a", "${local.region}b", "${local.region}c"]
+  vpc_id              = module.vpc.id
+  type                = "public-private"
+  igw_id              = module.vpc.igw_id
+  cidr_block          = local.vpc_cidr_block
+  ipv6_cidr_block     = module.vpc.ipv6_cidr_block
+  enable_ipv6         = false
+
+  extra_public_tags = {
+    "kubernetes.io/cluster/${module.eks.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                           = "1"
+  }
+
+  extra_private_tags = {
+    "kubernetes.io/cluster/${module.eks.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"                  = "1"
+  }
+
+}
+
+module "ssh" {
+  source      = "cypik/security-group/aws"
+  version     = "1.0.1"
+  name        = "${local.name}-ssh"
+  environment = local.environment
+  vpc_id      = module.vpc.id
+  new_sg_ingress_rules_with_cidr_blocks = [{
+    rule_count  = 1
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = [local.vpc_cidr_block]
+    description = "Allow ssh traffic."
+    }
+  ]
+
+  ## EGRESS Rules
+  new_sg_egress_rules_with_cidr_blocks = [{
+    rule_count  = 1
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh outbound traffic."
+  }]
+}
+
+
+module "http_https" {
+  source  = "cypik/security-group/aws"
+  version = "1.0.1"
+
+  name        = "${local.name}-http-https"
+  environment = local.environment
+
+  vpc_id = module.vpc.id
+  ## INGRESS Rules
+  new_sg_ingress_rules_with_cidr_blocks = [
+    {
+      rule_count  = 2
+      from_port   = 80
+      protocol    = "tcp"
+      to_port     = 80
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow http traffic."
+    },
+    {
+      rule_count  = 3
+      from_port   = 443
+      protocol    = "tcp"
+      to_port     = 443
+      cidr_blocks = ["0.0.0.0/0"]
+      description = "Allow https traffic."
+    }
+  ]
+
+  ## EGRESS Rules
+  new_sg_egress_rules_with_cidr_blocks = [{
+    rule_count       = 1
+    from_port        = 0
+    protocol         = "-1"
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    description      = "Allow all traffic."
+    }
+  ]
+}
+
+module "kms" {
+  source              = "cypik/kms/aws"
+  version             = "1.0.1"
+  name                = "${local.name}-kms"
+  environment         = local.environment
+  enabled             = true
+  description         = "KMS key for EBS of EKS nodes"
+  enable_key_rotation = false
+  policy              = data.aws_iam_policy_document.kms.json
+}
+
+data "aws_iam_policy_document" "kms" {
+  version = "2012-10-17"
+  statement {
+    sid    = "Enable IAM User Permissions"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+
+module "eks" {
+  source      = "cypik/eks/aws"
+  version     = "1.0.4"
+  enabled     = true
+  name        = local.name
+  environment = local.environment
+
+  # EKS
+  kubernetes_version     = "1.29"
+  endpoint_public_access = true
+  # Networking
+  vpc_id                            = module.vpc.id
+  subnet_ids                        = module.subnets.private_subnet_id
+  allowed_security_groups           = [module.ssh.security_group_id]
+  eks_additional_security_group_ids = [module.ssh.security_group_id, module.http_https.security_group_id]
+  allowed_cidr_blocks               = [local.vpc_cidr_block]
+
+  managed_node_group_defaults = {
+    subnet_ids                          = module.subnets.private_subnet_id
+    nodes_additional_security_group_ids = [module.ssh.security_group_id]
+    tags = {
+      "kubernetes.io/cluster/${module.eks.cluster_name}" = "shared"
+      "k8s.io/cluster/${module.eks.cluster_name}"        = "shared"
+    }
+    block_device_mappings = {
+      xvda = {
+        device_name = "/dev/xvda"
+        ebs = {
+          volume_size = 50
+          volume_type = "gp3"
+          iops        = 3000
+          throughput  = 150
+          encrypted   = true
+          kms_key_id  = module.kms.key_arn
+        }
+      }
+    }
+  }
+  managed_node_group = {
+    spot = {
+      name           = "${module.eks.cluster_name}-spot"
+      capacity_type  = "SPOT"
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 1
+      instance_types = ["t3.medium"]
+    }
+
+    #    on_demand = {
+    #      name                 = "${module.eks.cluster_name}-on-demand"
+    #      capacity_type        = "ON_DEMAND"
+    #      min_size             = 1
+    #      max_size             = 2
+    #      desired_size         = 1
+    #      force_update_version = true
+    #      instance_types       = ["t3.medium"]
+    #    }
+  }
+
+  apply_config_map_aws_auth = true
+  map_additional_iam_users = [
+    {
+      userarn  = "arn:aws:iam::123456789:user/cypik"
+      username = "test"
+      groups   = ["system:masters"]
+    }
+  ]
+}
+
+## Kubernetes provider configuration
+data "aws_eks_cluster_auth" "this" {
+  depends_on = [module.eks]
+  name       = module.eks.cluster_id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = join("", data.aws_eks_cluster_auth.this[*].token)
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = module.eks.cluster_certificate_authority_data
+  token                  = join("", data.aws_eks_cluster_auth.this.token)
+}
+
+module "splunk" {
+  source           = "./../../"
+  name             = "splunk"
+  chart            = "splunk-connect-for-kubernetes"
+  namespace        = "splunk-kubernetes-logging"
+  repository       = "https://splunk.github.io/splunk-connect-for-kubernetes/"
+  chart_version    = "1.5.4"
+  create_namespace = true
+  set = [
+    {
+      name  = "clusterName"
+      value = module.eks.cluster_name
+    },
+    {
+      name  = "vpcId"
+      value = module.vpc.id
+    },
+    {
+      name  = "region"
+      value = "us-west-2"
+    },
+    {
+      name  = "containers.logFormatType"
+      value = "cri"
+    },
+    {
+      name  = "splunk.hec.host"
+      value = "http-inputs-example.com"
+    },
+    {
+      name  = "splunk.hec.port"
+      value = "443"
+    },
+    {
+      name  = "splunk.hec.token"
+      value = "564864686543147"
+    },
+    {
+      name  = "splunk.hec.protocol"
+      value = "https"
+    },
+    {
+      name  = "splunk.hec.insecureSSL"
+      value = "true"
+    },
+    {
+      name  = "splunk-kubernetes-metrics.enabled"
+      value = "false"
+    },
+    {
+      name  = "splunk-kubernetes-objects.enabled"
+      value = "false"
+    },
+  ]
+}
---
+++ example/splunk/versions.tf
@@ -0,0 +1,31 @@
+# Terraform version
+terraform {
+  required_version = ">= 1.5.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.11.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.10.0, < 2.0.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 2.0.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.3.0, < 3.0.0" # Specify the version constraint here
+    }
+  }
+}
